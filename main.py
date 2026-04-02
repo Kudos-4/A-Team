a0bd91517fe228c03d4fc6779cf8a4b08c637d86
ppiepp <sssunchhh@gmail.com>
2026-04-02 19:25:20 -0400

    Initialize database connection before UI launch
    Added database initialization before running the UI.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """Main injection for running game in UI"""
 
 from checkers.user_interface.ui import CheckersUserInterface
-
+from checkers.auth.database import init_db
 
 def main() -> None:
-    """Main runs UI function, UI imports game to display and interact with"""
+    """
+    Main runs UI function, UI imports game to display and interact with.
+    Called init_db() from /auth/database.py before UI, ensure db connection.
+    """
+    init_db()
     RESOLUTION = (1920, 1080)
     ui = CheckersUserInterface(RESOLUTION)
     ui.mainloop()
