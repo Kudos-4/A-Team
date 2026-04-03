cebafacc1246da2b59ef5763b443c76c11489972
Nikolai-02 <nik.huffman02@gmail.com>
2026-04-03 13:58:36 -0400

    Added Update_piece and remove_piece

--- checkers/game/board.py
+++ checkers/game/board.py
@@ -99,8 +99,15 @@ class Board:
     def update_piece(
         self, position: tuple[int, int], new_piece: Piece
     ) -> None:
-        """Replaces a piece at a tile. Use this for promotions to new piece."""
-        raise NotImplementedError()
+        tile = self._tile_at(position)
+        if tile.piece is None:
+            raise ValueError("No piece at position to replace.")
+        tile.piece = new_piece
+    def remove_piece(self, position: tuple[int, int]) -> None:
+        tile = self._tile_at(position)
+        if tile.piece is None:
+            raise ValueError("No piece at position to be removed")
+        tile.piece = None
 
     def _tile_at(self, position: tuple[int, int]) -> Tile:
         row, col = position
